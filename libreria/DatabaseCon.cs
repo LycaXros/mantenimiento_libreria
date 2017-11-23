d1ef1ffb7c47a3cd7e46bd4f566c1852f5378102
Jesus Francisco Dicent Aguero <jesusdicent@gmail.com>
2017-11-22 23:21:28 -0400

    Prueba
    No se que hago

---
+++ libreria/DatabaseCon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+
+public class DatabaseCon
+{
+	
+	private DatabaseCon _Instance;
+	public SqlConnection Connection;
+
+	public DatabaseCon Instancia
+	{
+		get {
+			if (_Instance == null)
+				_Instance = new DatabaseCon();
+			return _Instance;
+		}
+
+	}
+
+
+	private DatabaseCon()
+	{
+		Connection = new SqlConnection();
+	}
+}
